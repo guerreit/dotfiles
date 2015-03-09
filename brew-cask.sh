adf9a1b865a3bae55d492e7283f18deb2281d472
Garrett Jones <garrettlouisjones@gmail.com>
2015-03-09 11:14:26 -0500

    hidden files

--- brew-cask.sh
+++ brew-cask.sh
@@ -4,6 +4,7 @@ brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
 
 brew cask install atom
+brew cask isntall dash
 brew cask install divvy
 brew cask install flux
 brew cask install google-chrome
