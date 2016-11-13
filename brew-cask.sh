8d43ee203dd5a672e55a52840ff58b32da6db9e6
Garrett Jones <garrettjones@me.com>
2016-11-13 09:47:11 -0600

    update brew cask file

--- brew-cask.sh
+++ brew-cask.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+#!/bin/zsh
 
-brew tap caskroom/versions
-
+brew cask install 1password
 brew cask install appcleaner
 brew cask install atom
 brew cask install caffeine
@@ -9,7 +8,13 @@ brew cask isntall dash
 brew cask install divvy
 brew cask install flux
 brew cask install google-chrome
-brew cask install google-drive
-brew cask install spotify
-brew cask install totalterminal
+brew cask install hipchat
+brew cask install java
+brew cask install licecap
+brew cask install postman
+brew cask install robomongo
+brew cask install slack
+brew cask install sonos
+brew cask install sourcetree
+brew cask install transmission
 brew cask install vlc
