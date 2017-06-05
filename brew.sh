ac0ebd4964f2510dc47e3c933a75c9510afef35a
Garrett Jones <garrettj@slalom.com>
2017-06-05 17:18:21 -0500

    brews

--- brew.sh
+++ brew.sh
@@ -15,21 +15,11 @@ brew tap homebrew/versions
 # cask
 brew tap caskroom/cask
 
-#bash 4
-brew install bash
-brew install bash-completion
-brew install homebrew/completions/brew-cask-completion
-
+# casks i need
 brew install git
 brew install mongodb
 brew install node
 brew install z
-
-# Switch to using brew-installed bash as default shell
-if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
-  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-  chsh -s /usr/local/bin/bash;
-fi;
 
 # Remove outdated versions from the cellar
 brew cleanup
