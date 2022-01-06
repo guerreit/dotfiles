3d8f8a95704c4fd4e298e7fa8a96fc600190029f
Garrett Jones <garrettj@slalom.com>
2022-01-06 09:31:17 -0600

    adds firefox to brew casks

--- brew-cask.sh
+++ brew-cask.sh
@@ -6,9 +6,9 @@ brew tap homebrew/cask
 brew install --cask appcleaner
 brew install --cask calibre
 brew install --cask docker
+brew install --cask firefox
 brew install --cask postman
 brew install --cask slack
 brew install --cask visual-studio-code
 brew install --cask microsoft-teams
-brew install --cask microsoft-edge
 brew install --cask webstorm
