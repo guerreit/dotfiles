fde25653ac220704b3d8d0c889490cee0f629eae
Garrett Jones <garrettj@slalom.com>
2022-01-13 10:08:27 -0600

    updates brews

--- brew-cask.sh
+++ brew-cask.sh
@@ -3,12 +3,17 @@
 # cask
 brew tap homebrew/cask
 
-brew install --cask appcleaner
 brew install --cask calibre
 brew install --cask docker
 brew install --cask firefox
-brew install --cask postman
-brew install --cask slack
-brew install --cask visual-studio-code
+brew install --cask intellij-idea
+brew install --cask microsoft-auto-update
 brew install --cask microsoft-teams
+brew install --cask postman
+brew install --cask pycharm
+brew install --cask sip
+brew install --cask slack
+brew install --cask sourcetree
+brew install --cask visual-studio-code
 brew install --cask webstorm
+brew install --cask zoom
---
+++ osx.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Turn off animations for new windows
+defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
+
+# Turns off smooth scrolling
+defaults write -g NSScrollAnimationEnabled -bool false
+
+# Disable the over-the-top focus ring animation
+defaults write NSGlobalDomain NSUseAnimatedFocusRing -bool false
