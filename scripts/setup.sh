d07734d44661ba2e08b801565e7374d3efe43829
Garrett Jones <garrettj@slalom.com>
2024-04-24 15:45:50 -0500

    better setup script organization

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,11 +6,15 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 # Make scripts executable
 chmod u+x scripts/brews.sh
 chmod u+x scripts/casks.sh
-chmod u+x scripts/dotfiles.sh
 chmod u+x scripts/osx.sh
+chmod u+x scripts/sync.sh
 
-# sync files, install brews and casks
+# install brews and casks
 ./scripts/brews.sh
 ./scripts/casks.sh
-./scripts/dotfiles.sh
+
+# Light OSX manipulation
 ./scripts/osx.sh
+
+# sync dotfiles
+./scripts/sync.sh
