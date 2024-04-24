ad4568797bcd0db8efc0609d37f456bb9ce2656a
Garrett Jones <garrettj@slalom.com>
2024-04-24 14:28:29 -0500

    ensure setup works

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,13 +4,13 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Make scripts executable
-chmod u+x brews.sh
-chmod u+x casks.sh
-chmod u+x dotfiles.sh
-chmod u+x osx.sh
+chmod u+x scripts/brews.sh
+chmod u+x scripts/casks.sh
+chmod u+x scripts/dotfiles.sh
+chmod u+x scripts/osx.sh
 
 # sync files, install brews and casks
-./scripts/brew-cask.sh
-./scripts/brew.sh
+./scripts/brews.sh
+./scripts/casks.sh
 ./scripts/dotfiles.sh
 ./scripts/osx.sh
