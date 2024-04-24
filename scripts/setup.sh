961d2f718ea85791a6ceae9d71596ea2eecba3e0
Garrett Jones <garrettj@slalom.com>
2024-04-24 10:08:32 -0500

    better name for bootstrap; clean up aliases

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,13 +4,13 @@
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Make scripts executable
-chmod u+x bootstrap.sh
+chmod u+x copy-src.sh
 chmod u+x brew.sh
 chmod u+x brew-cask.sh
 chmod u+x osx.sh
 
 # sync files, install brews and casks
-./scripts/bootstrap.sh
+./scripts/copy-src.sh
 ./scripts/brew.sh
 ./scripts/brew-cask.sh
 ./scripts/osx.sh
