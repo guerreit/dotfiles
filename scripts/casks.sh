1d62ebaae0fca0ca468b9d2afc632aacf481e9e4
Garrett Jones <garrettj@slalom.com>
2024-04-29 08:32:11 -0500

    adds onenote; update casks first

--- scripts/casks.sh
+++ scripts/casks.sh
@@ -1,7 +1,10 @@
 #!/bin/zsh
 
+brew upgrade --cask
+
 brew install --cask microsoft-auto-update
 brew install --cask microsoft-edge
+brew install --cask microsoft-onenote
 brew install --cask microsoft-teams
 brew install --cask postman
 brew install --cask slack
